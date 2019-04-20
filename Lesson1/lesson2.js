5afae93ce485969585d5734d3c3f82101fdea727
webNastya <haskibob99@gmail.com>
2019-04-20 18:11:48 +0300

    Lesson2

--- Lesson1/lesson2.js
+++ Lesson1/lesson2.js
@@ -12,16 +12,17 @@ let money = +prompt("Ваш бюджет на месяц?", ''),
         savings: false
     };
 
-// for(let i = 0; i < 2; i++) {
-//     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-//         b = prompt("Во сколько обойдется?", '');
-//     if (typeof(a) === 'string' && (typeof(a)) != null && (typeof(b)) != null
-//          && a != '' && b != '' && a.length < 50) {
-//         appData.expenses[a] = b;
-//     } else{
-//         appData.expenses[a] = b;
-//     }
-// };
+for(let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+    if (typeof(a) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+         && a != '' && b != '' && a.length < 50) {
+        appData.expenses[a] = b;
+    } else{
+        appData.expenses[a] = b;
+    }
+};
+
 
 // let i = 0;
 // while (i < 2) {
@@ -36,18 +37,19 @@ let money = +prompt("Ваш бюджет на месяц?", ''),
 //     i++;
 // }
 
-let i = 0;
-do {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-        b = prompt("Во сколько обойдется?", '');
-    if (typeof(a) === 'string' && (typeof(a)) != null && (typeof(b)) != null
-         && a != '' && b != '' && a.length < 50) {
-        appData.expenses[a] = b;
-    } else{
-        appData.expenses[a] = b;
-    }
-    i++;
-} while (i < 2);
+
+// let i = 0;
+// do {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//         b = prompt("Во сколько обойдется?", '');
+//     if (typeof(a) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+//          && a != '' && b != '' && a.length < 50) {
+//         appData.expenses[a] = b;
+//     } else{
+//         appData.expenses[a] = b;
+//     }
+//     i++;
+// } while (i < 2);
 
 appData.moneyPerDay = appData.appMoney / 30;
 
