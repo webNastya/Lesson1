f72753aaeee65e7eea6f870e40b74cc180b06271
webNastya <haskibob99@gmail.com>
2019-04-22 23:03:37 +0300

    new homework

--- Lesson1/lesson-dop2.js
+++ Lesson1/lesson-dop2.js
@@ -2,13 +2,17 @@
 
 let week = [ "Понедельник", "Вторник", "Среда", "Четверг",
                  "Пятница", "Суббота", "Восресенье"];
-for (let count = 0; count < week.length; count++) {
+for (let count = 0; count < 5; count++) {
         document.write( week[count] + "<br>");
-    if (week[count] == week[5]) {
-        document.write( week[5] + "</br>" + "<b><b/>");
-    }
-    if (week[count] == week[6]) {
-        document.write( week[6] + "</br>" + "<b><b/>");
-    }
     
+    // if (week[count] == week[5]) {
+    //     document.write( "<b><b/>" + "</br>" );
+    // }
+    // if (week[count] == week[5]) {
+    //     document.write( week[6] + "</br>" + "<b><b/>");
+    // }
+    
+}
+for (let i = 4; i >= 6; i++) {
+        document.write( week[i] + "<b><b/>" + "<br>");
 }
