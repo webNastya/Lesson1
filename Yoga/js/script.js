10f26a51b2cb16a15ba5bfdf6d724bacb68bb02f
webNastya <haskibob99@gmail.com>
2019-05-06 23:03:57 +0300

    lesson9

--- Yoga/js/script.js
+++ Yoga/js/script.js
@@ -77,33 +77,34 @@ window.addEventListener('DOMContentLoaded', function () {
 
 	//Modal
 
-	function modalMore() {
-		let more = document.querySelector('.more'),
-			overlay = document.querySelector('.overlay'),
-			descriptionBtn = document.querySelectorAll('.description-btn'),
-			close = document.querySelector('.popup-close');
+	let more = document.querySelector('.more'),
+		overlay = document.querySelector('.overlay'),
+		descriptionBtn = document.querySelectorAll('.description-btn'),
+		close = document.querySelector('.popup-close');
 
-			more.addEventListener('click', function() {
+	function modalMore() {
+		more.addEventListener('click', function() {
+			overlay.style.display = 'block';
+			this.classList.add('more-splash');
+			document.body.style.overflow = 'hidden';
+		});
+
+		for (let i = 0; i < descriptionBtn.length; i++) {
+			descriptionBtn[i].addEventListener('click', function() {
 				overlay.style.display = 'block';
 				this.classList.add('more-splash');
 				document.body.style.overflow = 'hidden';
 			});
-
-			for (let i = 0; i < descriptionBtn.length; i++) {
-				descriptionBtn[i].addEventListener('click', function() {
-					overlay.style.display = 'block';
-					this.classList.add('more-splash');
-					document.body.style.overflow = 'hidden';
-				});
-			}
-			more = overlay;
-
-			close.addEventListener('click', function(){
-				overlay.style.display = 'none';
-				more.classList.remove('more-splash');
-				document.body.style.overflow = '';
-			});
+		}
+	}
+	function closeModal() {
+		close.addEventListener('click', function(){
+			overlay.style.display = 'none';
+			more.classList.remove('more-splash');
+			document.body.style.overflow = '';
+		});
 	};
 	modalMore();
+	closeModal();
 
 });
