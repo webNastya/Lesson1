3b85e8af347b93cca6aa2bcd17f3564a841f739d
webNastya <haskibob99@gmail.com>
2019-05-08 12:46:53 +0300

    lesson11

--- Yoga/js/script.js
+++ Yoga/js/script.js
@@ -107,4 +107,91 @@ window.addEventListener('DOMContentLoaded', function () {
 	modalMore();
 	closeModal();
 
+	// Form
+
+	let message = {
+		loading: 'Загрузка...',
+		success: 'Спасибо! Скоро мы с вами свяжемся!',
+		failure: 'Что-то пошло не так'
+	};
+
+	let form = document.querySelector('.main-form'),
+		input = form.getElementsByTagName('input'),
+		formContact = document.querySelector('#form'),
+		inputs = formContacts.getElementsByTagName('input'),
+		statusMessage = document.createElement('div');
+
+		statusMessage.classList.add('status');
+
+	form.addEventListener('submit', function(event) {
+		event.preventDefault();
+		form.appendChild(statusMessage);
+
+		let request = new XMLHttpRequest();
+		request.open('POST', 'server.php');
+		request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+		let formData = new FormData(form);
+
+		let obj = {};
+		formData.forEach(function(value, key){
+			obj[key] = value;
+		});
+		let json = JSON.stringify(obj);
+
+		request.send(json);
+
+		request.addEventListener('readystatechange', function(){
+			if (request.readyState < 4) {
+				statusMessage.innerHTML = message.loading;
+			} else if (request.readyState === 4 && request.status == 200) {
+				statusMessage.innerHTML = message.success;
+			} else {
+				statusMessage.innerHTML = message.failure;
+			}
+		});
+		for (let i = 0; i < input.length; i++) {
+			input[i]. value = '';
+		}
+	});
+	
+
+	formContact.addEventListener('submit', function(event) {
+		event.preventDefault();
+		formContact.appendChild(statusMessage);
+	});
+
+	// Form contacts
+
+	formContact.addEventListener('submit', function(event) {
+		event.preventDefault();
+		formContact.appendChild(statusMessage);
+
+		let request = new XMLHttpRequest();
+		request.open('POST', 'server.php');
+		request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+		let formData = new FormData(formContact);
+
+		let obj = {};
+		formData.forEach(function(value, key){
+			obj[key] = value;
+		});
+		let json = JSON.stringify(obj);
+
+		request.send(json);
+
+		request.addEventListener('readystatechange', function(){
+			if (request.readyState < 4) {
+				statusMessage.innerHTML = message.loading;
+			} else if (request.readyState === 4 && request.status == 200) {
+				statusMessage.innerHTML = message.success;
+			} else {
+				statusMessage.innerHTML = message.failure;
+			}
+		});
+		for (let i = 0; i < inputs.length; i++) {
+			inputs[i]. value = '';
+		}
+	});
 });
