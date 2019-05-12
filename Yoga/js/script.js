1eae818a9b77d6b6bba0465e7a0312d0e546a57a
webNastya <haskibob99@gmail.com>
2019-05-13 01:05:36 +0300

    12

--- Yoga/js/script.js
+++ Yoga/js/script.js
@@ -120,54 +120,63 @@
 		formContact = document.querySelector('#form'),
 		inputs = formContact.getElementsByTagName('input'),
 		statusMessage = document.createElement('div');
-
 		statusMessage.classList.add('status');
 
-	form.addEventListener('submit', function(event) {
-		event.preventDefault();
-		form.appendChild(statusMessage);
+	function sendForm(elem) {
+		elem.addEventListener('submit', function(event) {
+			event.preventDefault();
+			form.appendChild(statusMessage);
+			let formData = new FormData(form);
 
-		let request = new XMLHttpRequest(),
-			formData = new FormData(form),
-			obj = {};
+			function postData(data){
+				return new Promise(function (resolve, reject) {
+					let request = new XMLHttpRequest();
+					request.open('POST', 'server.php');
+					request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
 
-		request.open('POST', 'server.php');
-		request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
-
-		formData.forEach(function(value, key){
-			obj[key] = value;
-		});
-		let json = JSON.stringify(obj);
-
-		request.send(json);
-
-		request.addEventListener('readystatechange', function(){
-			if (request.readyState < 4) {
-				statusMessage.innerHTML = message.loading;
-			} else if (request.readyState === 4 && request.status == 200) {
-				statusMessage.innerHTML = message.success;
-			} else {
-				statusMessage.innerHTML = message.failure;
+					request.onreadystatechange = function(){
+						if (request.readyState < 4) {
+							resolve()
+						} else if (request.readyState === 4 && request.status == 200) {
+							if (request.status === 200 && request.status < 3) {
+								resolve()
+							} else {
+								reject()
+							}
+						}
+					}
+					request.send(data);
+				})
 			}
-		});
-		for (let i = 0; i < input.length; i++) {
-			input[i]. value = '';
-		}
-	});
-	
 
-	formContact.addEventListener('submit', function(event) {
-		event.preventDefault();
-		formContact.appendChild(statusMessage);
-	});
-	let phone = document.getElementsByName('phone');
-	for (let i =0; i<phone.length; i++) {
-		phone[i].addEventListener('keypress', function (e) {
-        if (!/\d/.test(e.key) && !/\+/.test(e.key)) {
-            e.preventDefault();
-        }
-    });
+			function clearInput(){
+				for (let i = 0; i < input.length; i++) {
+					input[i]. value = '';
+				}
+			}
+
+			postData(formData)
+				.then(()=> statusMessage.innerHTML = message.loading)
+				.then(()=>{
+					thanksModal.style.display = 'block';
+					mainModal.style.display = 'none';
+					statusMessage.innerHTML = '';
+				})
+				.catch(()=> statusMessage.innerHTML = message.failure)
+				.then(clearInput)		
+		});
+		let phone = document.getElementsByName('phone');
+		for (let i =0; i<phone.length; i++) {
+			phone[i].addEventListener('keypress', function (e) {
+		        if (!/\d/.test(e.key) && !/\+/.test(e.key)) {
+		            e.preventDefault();
+		        }
+	    	});
+		}
 	}
+
+	sendForm(form);
+	sendForm(formContact);
 	
 	// Form contacts
 
